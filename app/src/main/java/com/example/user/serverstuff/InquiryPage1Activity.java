f8305e6b5be59cb8e5a5cd1a45ce271e75340416
Erik Burka <eriks.burka@ucdconnect.ie>
2019-03-12 10:32:16 +0000

    Splash Screen and First inquiry page

--- app/src/main/java/com/example/user/serverstuff/InquiryPage1Activity.java
+++ app/src/main/java/com/example/user/serverstuff/InquiryPage1Activity.java
@@ -1,13 +1,72 @@
 package com.example.user.serverstuff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class InquiryPage1Activity extends AppCompatActivity {
+
+    private static String province;
+    private Button leinster, munster, connacht, ulster;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inquiry_page1);
+
+        leinster = findViewById(R.id.leinster);
+        munster = findViewById(R.id.munster);
+        connacht = findViewById(R.id.connacht);
+        ulster = findViewById(R.id.ulster);
+        textView = findViewById(R.id.textView1);
+
+        leinster.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setProvince("leinster");
+                Intent i = new Intent(getApplicationContext(),InquiryPage2Activity.class);
+                startActivity(i);
+                textView.setText(getProvince());
+            }
+        });
+
+        munster.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setProvince("munster");
+                Intent i = new Intent(getApplicationContext(),InquiryPage2Activity.class);
+                startActivity(i);
+                textView.setText(getProvince());
+            }
+        });
+
+        connacht.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setProvince("connacht");
+                Intent i = new Intent(getApplicationContext(),InquiryPage2Activity.class);
+                startActivity(i);
+                textView.setText(getProvince());
+            }
+        });
+
+        ulster.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setProvince("ulster");
+                Intent i = new Intent(getApplicationContext(),InquiryPage2Activity.class);
+                startActivity(i);
+                textView.setText(getProvince());
+            }
+        });
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getProvince() {
+
+        return province;
     }
 }
--- app/src/main/java/com/example/user/serverstuff/SplashScreenActivity.java
+++ app/src/main/java/com/example/user/serverstuff/SplashScreenActivity.java
@@ -1,13 +1,32 @@
 package com.example.user.serverstuff;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, InquiryPage1Activity.class);
+                startActivity(i);
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
     }
 }
+
