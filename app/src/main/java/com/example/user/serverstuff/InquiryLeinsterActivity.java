630837be86f9aa3daf2b0ad6db3497afed1431b2
Erik Burka <eriks.burka@ucdconnect.ie>
2019-03-13 16:31:50 +0000

    Clean Up and First 2 activities with buttons

---
+++ app/src/main/java/com/example/user/serverstuff/InquiryLeinsterActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.serverstuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class InquiryLeinsterActivity extends AppCompatActivity {
+
+    private Button carlow, dublin, kildare, killkeny, laois, longford, louth, meath, offaly, westmeath, wexford, wicklow, continueButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inquiry_leinster);
+    }
+}
---
+++ app/src/main/java/com/example/user/serverstuff/InquiryMunsterActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.serverstuff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InquiryMunsterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inquiry_munster);
+    }
+}
