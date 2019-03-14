26e89b448391533b09d9e7d51f90816d1992215e
Erik Burka <eriks.burka@ucdconnect.ie>
2019-03-14 11:29:23 +0000

    Working button in Leinster

--- app/src/main/java/com/example/user/serverstuff/InquiryLeinsterActivity.java
+++ app/src/main/java/com/example/user/serverstuff/InquiryLeinsterActivity.java
@@ -1,7 +1,9 @@
 package com.example.user.serverstuff;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class InquiryLeinsterActivity extends AppCompatActivity {
@@ -12,5 +14,16 @@ public class InquiryLeinsterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inquiry_leinster);
+
+        dublin = findViewById(R.id.Dublin);
+
+        dublin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
